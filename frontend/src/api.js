77c78cf87332ea7c4e7a4472d4848179c4bc845c
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2024-08-11 18:24:18 +0800

    Add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,8 +3,9 @@
 import axios from "axios"
 import { ACCESS_TOKEN  } from "./constants"
 
+const apiUrl = "/choreo-apis/manga/api/v1"
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(
