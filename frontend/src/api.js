b821b08987d56c9f192c1b56857874ce5a0fa931
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2024-08-13 18:03:39 +0800

    Update requirements.txt

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,7 +5,7 @@ import { ACCESS_TOKEN  } from "./constants"
 
 const apiUrl = "/choreo-apis/manga/backend/v1"
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+    baseURL: apiUrl,
 
 })
 
